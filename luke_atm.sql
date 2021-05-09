f3e0fcc45677d747ea4658acd8779fc5c9cc9cae
Sabo Luka <sabolukas03@gmail.com>
2021-05-09 18:30:07 +0200

    fix: Possible fix for transactions not showing

--- luke_atm.sql
+++ luke_atm.sql
@@ -1,9 +1,9 @@
 CREATE TABLE transactions (
   id int(11) NOT NULL AUTO_INCREMENT,
-  identifier varchar(40) NOT NULL,
+  identifier varchar(50) NOT NULL,
   comment tinytext DEFAULT NULL,
   amount int(11) NOT NULL,
-  recipient varchar(40) DEFAULT NULL,
+  recipient varchar(50) DEFAULT NULL,
   type tinytext DEFAULT NULL,
   PRIMARY KEY (id)
 );
